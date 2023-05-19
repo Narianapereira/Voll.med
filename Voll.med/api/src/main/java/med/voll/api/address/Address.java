64c38d33a5cdc1085602bbf315ee14f8f3ee18f4
Narianapereira <nariana.pereira@outlook.com>
2023-05-19 08:21:09 -0300

    Created database entities with JPA

---
+++ Voll.med/api/src/main/java/med/voll/api/address/Address.java
@@ -0,0 +1,21 @@
+package med.voll.api.address;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Address {
+
+    private String street;
+    private String neighborhood;
+    private String cep;
+    private String city;
+    private String state;
+    private String extra;
+    private String number;
+}
