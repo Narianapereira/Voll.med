b0618832531387f2e7de31620836f4b651d2d58f
nariana <nariana.pereira@outlook.com>
2023-09-17 19:35:14 -0300

    Access control with filter

--- Voll.med/api/src/main/java/med/voll/api/infra/security/TokenService.java
+++ Voll.med/api/src/main/java/med/voll/api/infra/security/TokenService.java
@@ -3,6 +3,9 @@ package med.voll.api.infra.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.auth0.jwt.interfaces.JWTVerifier;
 import med.voll.api.domain.user.User;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -35,5 +38,20 @@ public class TokenService {
     private Instant dateExpire() {
        return LocalDateTime.now().plusHours(2)
                .toInstant(ZoneOffset.of("-03:00"));
+    }
+
+    public String validateToken(String tokenJWT){
+        DecodedJWT decodedJWT;
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("Api Voll.med")
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+
+        } catch (JWTVerificationException exception){
+            throw new RuntimeException("Expired or invalid token");
+        }
     }
 }
